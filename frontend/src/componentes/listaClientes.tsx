2a3ab72c4f9131fd8ba4a460d48af0ada8097986
Silmara Bittencourt <silmarabittencourt13@gmail.com>
2024-06-12 09:03:13 -0300

    Aparece na lista de clientes mais ou menos formatado

--- frontend/src/componentes/listaClientes.tsx
+++ frontend/src/componentes/listaClientes.tsx
@@ -1,14 +1,19 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import { Link } from 'react-router-dom';
 
 interface Cliente {
   id: number;
   nome: string;
+  cpf: string;
+  rg: string;
+  telefone: []; 
 }
 
 const ListaCliente = () => {
   const [clientes, setClientes] = useState<Cliente[]>([]);
   const [error, setError] = useState<string | null>(null);
+  const [clienteSelecionadoIndex, setClienteSelecionadoIndex] = useState<number | null>(null);
 
   useEffect(() => {
     const fetchData = async () => {
@@ -19,7 +24,6 @@ const ListaCliente = () => {
         if (response.status === 302) {
           setClientes(response.data);
         } 
-        
       } catch (error) {
         setError('Erro ao obter clientes. Por favor, tente novamente mais tarde.');
       }
@@ -27,18 +31,54 @@ const ListaCliente = () => {
   
     fetchData();
   }, []);
-  
-  
+
+  const handleClick = (index: number) => {
+    setClienteSelecionadoIndex(clienteSelecionadoIndex === index ? null : index);
+  };
+
+  const handleCadastroCliente = () => {
+  };
+
+  const handleExcluirCliente = (index: number) => {
+
+  };
+
+  const handleAtualizarCliente = (index: number) => {
+
+    
+  };
 
   return (
-    <div>
-      <h1>Lista de Clientes</h1>
-      {error && <p>{error}</p>}
-      <ul>
-        {clientes.map(cliente => (
-          <li key={cliente.id}>{cliente.nome}</li>
+    <div className="container-fluid">
+      <div className="d-flex justify-content-start mb-3">
+        <Link to="/cadastro" className="btn btn-primary" onClick={handleCadastroCliente}>Cadastrar novo cliente</Link>
+      </div>
+      <div className="list-group">
+        {clientes.map((cliente, index) => (
+          <div key={index}>
+            <button
+              className="list-group-item list-group-item-action"
+              onClick={() => handleClick(index)}
+            >
+              {cliente.nome}
+            </button>
+            {clienteSelecionadoIndex === index && (
+              <div className="card mt-3">
+                <div className="card-body">
+                  <h5 className="card-title">{cliente.nome}</h5>
+                  <p className="card-text">CPF: {cliente.cpf}</p>
+                  <p className="card-text">RG: {cliente.rg}</p>
+                  <p className="card-text">Telefone: {cliente.telefone}</p>
+                  <div className="mt-3">
+                    <button className="btn btn-danger btn-sm ml-2" onClick={() => handleExcluirCliente(index)}>Excluir</button>
+                    <button className="btn btn-primary btn-sm ml-2" onClick={() => handleAtualizarCliente(index)}>Atualizar</button>
+                  </div>
+                </div>
+              </div>
+            )}
+          </div>
         ))}
-      </ul>
+      </div>
     </div>
   );
 };
